afde619d064b2c5c0c65d2f4f86f380de89fcb65
DarBaala <joker.milan1997gmail.com>
2021-12-28 18:13:52 +0300

    added JS

---
+++ main.js
@@ -0,0 +1,9 @@
+const money = "money me";
+const income = "income";
+const addExpenses = "addExpenses";
+const deposit = "deposit h";
+const mission = "mission";
+const period = "const";
+
+alert(money);
+console.log(deposit);
