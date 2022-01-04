ffbe6cf21bb118ca817ac422d4bce9bbecda0d03
DarBaala <joker.milan1997gmail.com>
2022-01-04 10:19:16 +0300

    upd

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-const money = "money me";
-const income = "income";
-const addExpenses = "addExpenses";
-const deposit = "deposit h";
-const mission = "mission";
-const period = "const";
-
-alert(money);
-console.log(deposit);
+"use strict";
+let money;
+let income;
+let addExpenses;
+let deposit;
+let mission;
+let period;
