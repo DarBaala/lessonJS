9424a3193de134846f888384d2970aea829b43f9
DarBaala <joker.milan1997gmail.com>
2022-01-06 17:49:08 +0300

    upd

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 "use strict";
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 30000;
+let income = "Фриланс: 2500";
+let addExpenses = "Еда, такси, отдых";
+let deposit = 0;
+let mission = 100000;
+let period = 6;
+let num = 266219;
+let res = 1;
+num
+  .toString()
+  .split("")
+  .forEach(function (el) {
+    res *= el;
+  });
+console.log(res);
+
+console.log((res ** 3).toString().substring(0, 2));
