69d2bc27ef0e1ea1f6b360af508fcfe749f35fcd
DarBaala <joker.milan1997gmail.com>
2022-03-17 20:20:51 +0300

    fixed

--- timer/index.js
+++ timer/index.js
@@ -1,10 +1,10 @@
 "use strict";
 
-const twoDigits = (num) => {
+function twoDigits(num) {
   return ("0" + num).slice(-2);
-};
+}
 
-const currentDate = document.getElementById("today");
+const currentDate = document.querySelector("#today");
 const days = [
   "Понедельник",
   "Вторник",
@@ -15,24 +15,24 @@ const days = [
   "Воскресенье",
 ];
 
-const timeFormat = () => {
-  const date = new Date();
-  const day = days[new Date().getDay() - 1];
-  const hour = date.getHours();
-  let greeting = "";
+function timeFormat() {
+  let date = new Date(),
+    day = days[new Date().getDay() - 1],
+    hour = date.getHours(),
+    greeting = "";
   if (hour >= 5 && hour < 12) {
-    greeting = "Доброе утро";
+    greeting = "Хуй ты проверишь в это время";
   } else if (hour >= 12 && hour < 18) {
-    greeting = "Добрый день";
+    greeting = "Добрый день, Ваша";
   } else if (hour >= 18 && hour < 24) {
-    greeting = "Добрый вечер";
+    greeting = "Cуьйр дик йойл, Ваша";
   } else if (hour >= 0 && hour < 5) {
-    greeting = "Доброй ночи";
+    greeting = "Д1авижа, ахь х1у джава юьйц....";
   }
-  const amPM = hour >= 12 ? "PM" : "AM";
-  const hours = hour > 12 ? hour - 12 : hour;
-  const minutes = twoDigits(date.getMinutes());
-  const seconds = twoDigits(date.getSeconds());
+  let amPM = hour >= 12 ? "PM" : "AM",
+    hours = hour > 12 ? hour - 12 : hour,
+    minutes = twoDigits(date.getMinutes()),
+    seconds = twoDigits(date.getSeconds());
   return {
     greeting,
     amPM,
@@ -41,9 +41,9 @@ const timeFormat = () => {
     minutes,
     seconds,
   };
-};
+}
 
-const getTimeRemaining = () => {
+function getTimeRemaining() {
   const dateStop = new Date("1 January 2023").getTime();
   const dateNow = new Date().getTime();
   const newYear = dateNow > dateStop ? true : false;
@@ -54,18 +54,18 @@ const getTimeRemaining = () => {
     day,
     newYear,
   };
-};
+}
 
-const textDate = () => {
+function textDate() {
   const time = timeFormat();
   const timer = getTimeRemaining();
   currentDate.innerHTML = `${time.greeting} <br>
   Сегодня: ${time.day} <br>
   Текущее время: ${twoDigits(time.hours)}:${time.minutes}:${time.seconds} ${
     time.amPM
-  }<br>
-  До нового года осталось ${timer.day} дней`;
-};
+  } <br>
+  До нового осталось ${timer.day} дней`;
+}
 if (getTimeRemaining().timeRemaining > 0) {
   textDate();
   const timerId = setInterval(() => {
