b33e46f8fef03a071d92dfa449f65fa73e18ba06
DarBaala <joker.milan1997gmail.com>
2022-03-20 22:25:11 +0300

    added 19 lesson

--- script/script.js
+++ script/script.js
@@ -45,16 +45,23 @@ window.addEventListener("DOMContentLoaded", function () {
   countTimer("22 march 2022");
   /// MENU
   const toggleMenu = () => {
-    const btnMenu = document.querySelector(".menu"),
-      menu = document.querySelector("menu"),
-      closeBtn = document.querySelector(".close-btn"),
-      menuItem = menu.querySelectorAll("ul>li");
+    const btnMenu = document.querySelector(".menu");
+    const menu = document.querySelector("menu");
     const handlerMenu = () => {
       menu.classList.toggle("active-menu");
     };
     btnMenu.addEventListener("click", handlerMenu);
-    closeBtn.addEventListener("click", handlerMenu);
-    menuItem.forEach((elem) => elem.addEventListener("click", handlerMenu));
+    menu.addEventListener("click", (event) => {
+      let target = event.target;
+      if (target.classList.contains("close-btn")) {
+        handlerMenu();
+      } else {
+        target = target.matches('[href^="#"]');
+        if (target) {
+          handlerMenu();
+        }
+      }
+    });
   };
   toggleMenu();
   ///POPUP
@@ -90,4 +97,34 @@ window.addEventListener("DOMContentLoaded", function () {
     });
   };
   togglePopUp();
+
+  //Таб
+  const tabs = () => {
+    const tabHeader = document.querySelector(".service-header");
+    const tab = document.querySelectorAll(".service-header-tab");
+    const tabContent = document.querySelectorAll(".service-tab");
+    const toggleTabContent = (index) => {
+      for (let i = 0; i < tabContent.length; i++) {
+        if (index === i) {
+          tab[i].classList.add("active");
+          tabContent[i].classList.remove("d-none");
+        } else {
+          tab[i].classList.remove("active");
+          tabContent[i].classList.add("d-none");
+        }
+      }
+    };
+    tabHeader.addEventListener("click", (event) => {
+      let target = event.target;
+      target = target.closest(".service-header-tab");
+      if (target) {
+        tab.forEach((item, i) => {
+          if (item === target) {
+            toggleTabContent(i);
+          }
+        });
+      }
+    });
+  };
+  tabs();
 });
